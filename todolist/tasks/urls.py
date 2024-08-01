1e825e014b0a47e6dc2bad05262deef8941fcb87
Sahil khan <116485673+SahilKhan44@users.noreply.github.com>
2024-08-01 18:24:01 +0530

    Python django todolist

---
+++ todolist/tasks/urls.py
@@ -0,0 +1,10 @@
+# tasks/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.task_list, name='task_list'),
+    path('add/', views.add_task, name='add_task'),
+    path('delete/<int:pk>/', views.delete_task, name='delete_task'),
+]
---
+++ todolist/todolist/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+     path('accounts/', include('django.contrib.auth.urls')), 
+    path('', include('tasks.urls')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+]
